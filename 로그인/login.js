574866a7297e47fc8453e045f9ee65c1e5ca33ff
yujunggg <a90134627@gmail.com>
2024-06-27 20:57:06 +0900

    login

---
+++ 로그인/login.js
@@ -0,0 +1,6 @@
+const login = () => {
+    const id = document.getElementById('id');
+    const passWord = document.getElementById('passWord');
+    console.log(id.value);
+    console.log(passWord.value);
+}
